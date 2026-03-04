18115b22dc79dae46c016da8a98a1ae4ada7c496
Nicholas <quackshows11@gmail.com>
2026-03-04 14:34:05 +0100

    feat: add two more projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -16,5 +16,21 @@ export const projects: Project[] = [
       "A mobile version of the classic Blackjack card game, built with React Native. Play against the dealer, use hit or stand strategies, and try to reach 21 without going bust!",
     githubUrl: "https://github.com/Clemond/Deck21",
     featured: false
+  },
+  {
+    title: "VoyageVault",
+    techStack: ["SwiftUI", "Firebase"],
+    description:
+      "An iOS application developed with a friend during our iOS development studies. This social media travel platform lets you explore vacation spots, save favorites, set countdowns for upcoming trips, and share travel photos with friends for them to see!",
+    githubUrl: "https://github.com/Navi9519/VoyageVaultProject",
+    featured: false
+  },
+  {
+    title: "ToDoApp",
+    techStack: ["React Native", "JavaScript", "Expo"],
+    description:
+      "A To-Do app developed with React Native. This app enables users to add tasks, mark them as completed, and delete them with ease. It focuses on essential React Native concepts such as state management, user input handling, and efficient list rendering",
+    githubUrl: "https://github.com/Clemond/ToDoAppReactNative",
+    featured: false
   }
 ];
