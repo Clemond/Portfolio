5b998e7bc42eb9329478bf654345f11d068c0077
Nicholas <quackshows11@gmail.com>
2026-03-19 13:14:33 +0100

    feat: add social media links and design to ContactSection

--- src/components/contact-section/ContactSection.tsx
+++ src/components/contact-section/ContactSection.tsx
@@ -1,6 +1,12 @@
+import SocialMediaLinks from "../presentation-section/SocialMediaLinks";
+import "./ContactSection.css";
+
 export default function ContactSection() {
   return (
-    <div>
+    <div className="contactSectionContainer">
+      <div className="contactSectionLinks">
+        <SocialMediaLinks />
+      </div>
       <p>© 2026 Nicholas Nieminen Jönsson</p>
     </div>
   );
