476d99218071b3b8c20622dd85b2040b13830158
Nicholas <quackshows11@gmail.com>
2026-03-23 14:31:34 +0100

    feat: map resume in ResumeSection

--- src/components/resume-section/ResumeSection.tsx
+++ src/components/resume-section/ResumeSection.tsx
@@ -1,15 +1,24 @@
+import { resume } from "../../data/resume";
 import "./ResumeSection.css";
 
 export default function ResumeSection() {
   return (
-    <div className="timeline">
-      <div className="timelineItem">
-        <div className="timelineLeft">2024</div>
-        <div className="timelineRight">
-          <h3>App Developer Intern</h3>
-          <p>Pixlo</p>
+    <div className="ResumeSectionTimeline">
+      <p className="ResumeSectionTitle">Resume</p>
+      {resume.map((job) => (
+        <div className="ResumeSectionTimelineItem">
+          <p className="ResumeSectionTimelineLeft">{job.year}</p>
+          <div className="ResumeSectionTimelineRight">
+            <h3>{job.title}</h3>
+            <p>{job.location}</p>
+            <p>
+              {job.description.map((desc) => (
+                <p> - {desc}</p>
+              ))}
+            </p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
