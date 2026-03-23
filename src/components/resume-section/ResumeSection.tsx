d3e8a37c41642ac1bc101bfa5f7ef91b8dd455ce
Nicholas <quackshows11@gmail.com>
2026-03-23 13:56:44 +0100

    feat: create ResumeSection tsx and css

---
+++ src/components/resume-section/ResumeSection.tsx
@@ -0,0 +1,15 @@
+import "./ResumeSection.css";
+
+export default function ResumeSection() {
+  return (
+    <div className="timeline">
+      <div className="timelineItem">
+        <div className="timelineLeft">2024</div>
+        <div className="timelineRight">
+          <h3>App Developer Intern</h3>
+          <p>Pixlo</p>
+        </div>
+      </div>
+    </div>
+  );
+}
