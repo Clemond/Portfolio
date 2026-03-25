94efb89dc9d5b0e135584ed6eca96ef90422be54
Nicholas <quackshows11@gmail.com>
2026-03-25 11:09:39 +0100

    chore: add a div around the resumeSection component

--- src/components/resume-section/ResumeSection.tsx
+++ src/components/resume-section/ResumeSection.tsx
@@ -3,22 +3,24 @@ import "./ResumeSection.css";
 
 export default function ResumeSection() {
   return (
-    <div id="resumeSection" className="ResumeSectionTimeline">
+    <div>
       <p className="ResumeSectionTitle">Resume</p>
-      {resume.map((job) => (
-        <div className="ResumeSectionTimelineItem">
-          <p className="ResumeSectionTimelineLeft">{job.year}</p>
-          <div className="ResumeSectionTimelineRight">
-            <h3>{job.title}</h3>
-            <p>{job.location}</p>
-            <p>
-              {job.description.map((desc) => (
-                <p> - {desc}</p>
-              ))}
-            </p>
+      <div id="resumeSection" className="ResumeSectionTimeline">
+        {resume.map((job) => (
+          <div className="ResumeSectionTimelineItem">
+            <p className="ResumeSectionTimelineLeft">{job.year}</p>
+            <div className="ResumeSectionTimelineRight">
+              <h3>{job.title}</h3>
+              <p>{job.location}</p>
+              <p>
+                {job.description.map((desc) => (
+                  <p> - {desc}</p>
+                ))}
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
